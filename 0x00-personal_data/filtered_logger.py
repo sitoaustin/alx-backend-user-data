6c40506d81be6d8ea9561d1ec2d9b7c48acf8f26
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-11-02 16:01:12 +0100

    Corrected the code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Regex-ing
+"""
+Definition of filter_datum function that returns an obfuscated log message
 """
 
 
@@ -11,5 +12,9 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str):
     """returns log message obfuscated
     """
-    pattern = "|".join(fields)
-    return re.sub(f'({pattern})=[^{separator}]+', f'\\1={redaction}', message)
+    # pattern = "|".join(fields)
+    # return re.sub(f'({pattern})=[^{separator}]+', f'\\1={redaction}', message)
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
