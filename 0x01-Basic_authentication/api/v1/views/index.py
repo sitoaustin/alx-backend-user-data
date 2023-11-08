3d0b3c087d1ef06e42f3bb7c882f1a6dc3f81995
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-11-08 18:54:30 +0100

    Fixed known errors

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,6 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-    abort(401)
+    abort(401, description="Unauthorized")
