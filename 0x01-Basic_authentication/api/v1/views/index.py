330fdf2a5516b89df183f3f0bac73b87863d3b0f
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-11-08 19:42:43 +0100

    Forbidden error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -36,7 +36,7 @@ def unauthorized() -> str:
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+def forbidden() -> str:
     """ GET /api/v1/forbidden
     Return:
       "{error: "forbidden}"
