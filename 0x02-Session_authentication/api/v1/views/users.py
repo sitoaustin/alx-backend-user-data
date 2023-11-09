33db790f8d9b93b06df58fa2bf9478f13390cee6
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-11-09 13:01:28 +0100

    Users/me

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -35,6 +35,8 @@ def view_one_user(user_id: str = None) -> str:
     user = User.get(user_id)
     if user is None:
         abort(404)
+    if request.current_user is None:
+        abort(404)
     return jsonify(user.to_json())
 
 
