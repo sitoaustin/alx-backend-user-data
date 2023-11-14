927dcffbff9649462b6ad30ce597fff29f6db4e6
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-11-14 12:07:06 +0100

    Minor change

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,9 +30,9 @@ def view_one_user(user_id: str = None) -> str:
     user = User.get(user_id)
     if user is None:
         abort(404)
-    if user_id is "me" and request.current_user is None:
+    if user_id == "me" and request.current_user is None:
         abort(404)
-    if user_id is "me" and request.current_user is not None:
+    if user_id == "me" and request.current_user is not None:
         return jsonify(user.to_json())
     return jsonify(user.to_json())
 
