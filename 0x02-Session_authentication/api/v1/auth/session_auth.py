94549d96ae5844d429a2a4831628f2c0efa07bb8
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-11-14 12:53:25 +0100

    Create session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,8 @@ Definition of class BasicAuth
 import base64
 from .auth import Auth
 from typing import TypeVar
+from uuid import uuid4
+
 
 from models.user import User
 
@@ -12,3 +14,19 @@ from models.user import User
 class SessionAuth(Auth):
     """ Implement Basic Authorization protocol methods
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user with id user_id
+        Args:
+            user_id (str): user's user id
+        Return:
+            None is user_id is None or not a string
+            Session ID in string format
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
