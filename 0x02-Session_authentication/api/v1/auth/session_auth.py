ce75ce92c3e17c3ca8205c4c787de24ba2d8fc28
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-11-14 12:48:28 +0100

    Session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Definition of class BasicAuth
+"""
+import base64
+from .auth import Auth
+from typing import TypeVar
+
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ Implement Basic Authorization protocol methods
+    """
+    
